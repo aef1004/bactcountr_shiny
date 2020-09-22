17f18734a3c7b968cc1b4d30ef96ab6bfff7edd1
Amy Fox <aef1004@gmail.com>
2020-09-22 11:59:39 -0600

    deploy the creating_input_files

--- writing/notes.R
+++ writing/notes.R
@@ -7,3 +7,9 @@
 # note that it is using ANOVA and Tukey HSD
 # add in a way to download .jpeg or .png of the printed plot
 # add in a way to download the cleaned CFU data
+
+# publish on this website
+#https://www.shinyapps.io/
+
+# To update on the website, just rerun the command:
+# rsconnect::deployApp("./writing/creating_input_files.Rmd")
