0f3f46aa156fd34299b7bb7aaf97f7125be08f00
Amy Fox <aef1004@gmail.com>
2020-09-22 11:48:38 -0600

    print the plots!

--- writing/notes.R
+++ writing/notes.R
@@ -1,2 +1,9 @@
 # https://shiny.rstudio.com/reference/shiny/latest/fileInput.html
 # good ideas on how to add in validation
+
+
+# other things to add
+
+# note that it is using ANOVA and Tukey HSD
+# add in a way to download .jpeg or .png of the printed plot
+# add in a way to download the cleaned CFU data
